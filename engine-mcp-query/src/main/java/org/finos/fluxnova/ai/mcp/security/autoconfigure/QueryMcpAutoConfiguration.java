1e123732862bc66fd367cde2ecd07cf895e05dad
Tom Stavert <tstavert@scottlogic.com>
2026-03-11 13:25:57 +0000

    Fixed autoconfig

--- engine-mcp-query/src/main/java/org/finos/fluxnova/ai/mcp/security/autoconfigure/QueryMcpAutoConfiguration.java
+++ engine-mcp-query/src/main/java/org/finos/fluxnova/ai/mcp/security/autoconfigure/QueryMcpAutoConfiguration.java
@@ -1,9 +1,12 @@
 package org.finos.fluxnova.ai.mcp.security.autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.AuthenticationProvider;
 
 @AutoConfiguration
+@ConditionalOnClass(AuthenticationProvider.class)
 @ComponentScan(basePackages = "org.finos.fluxnova.ai.mcp.security")
 public class QueryMcpAutoConfiguration {
 }

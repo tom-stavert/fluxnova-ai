1df59588008c630c64c0abedc1c1c4cd6295265e
bwright-scottlogic <bwright@scottlogic.com>
2026-03-11 10:23:48 +0000

    Add autoconfigure for security plugin.
    Signed-off-by: bwright-scottlogic <bwright@scottlogic.com>

---
+++ engine-mcp-query/src/main/java/org/finos/fluxnova/ai/mcp/security/autoconfigure/QueryMcpAutoConfiguration.java
@@ -0,0 +1,9 @@
+package org.finos.fluxnova.ai.mcp.security.autoconfigure;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "org.finos.fluxnova.ai.mcp.security")
+public class QueryMcpAutoConfiguration {
+}
